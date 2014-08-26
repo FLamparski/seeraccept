fd9e70e4cf937289aed4dfda13fd5a2888ed5a6e
Filip Wieland <filiplamparski@gmail.com>
2014-08-26 22:10:12 +0100

    get back here, cheerio

---
+++ packages/cheerio/cheerio.js
@@ -0,0 +1,1 @@
+cheerio = Npm.require('cheerio');
---
+++ packages/cheerio/package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: "cheerio",
+  summary: "node-cheerio."
+});
+
+Npm.depends({
+  "cheerio": "0.12.3"
+});
+
+Package.on_use(function(api) {
+  api.imply && api.imply('cheerio', ['server']);
+  api.add_files(['cheerio.js'], 'server');
+  api.export(['cheerio']);
+});
