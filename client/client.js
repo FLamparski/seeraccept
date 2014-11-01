f71ad489fd726efde5c59a58bc758d9d3791cee0
Filip Wieland <filiplamparski@gmail.com>
2014-11-01 18:02:23 +0000

    Header template helpers code cleanup

--- client/client.js
+++ client/client.js
@@ -166,20 +166,39 @@ Router.map(function() {
   });
 });
 
+
+Template.header.rendered = function(){
+  var self = this;
+  var oldPos = self.$('.dropdown-menu').offset();
+  var myWidth = self.$('.dropdown-menu').outerWidth();
+  var activatorWidth = self.$('.dropdown-toggle').outerWidth();
+  var newPos = { top: oldPos.top, left: activatorWidth - myWidth };
+  console.log('Moving the dropdown menu from (%d, %d) to (%d, %d)', oldPos.left,
+      oldPos.top, newPos.left, newPos.top);
+  self.$('.dropdown-menu').offset(newPos);
+};
+
+Template.header.helpers({
+  'status': Meteor.status
+});
+
 Template.header.events({
-    "click .refresh": function (e, tmpl) {
-        console.log("Check mail start...");
-        if(!Meteor.userId()){
-            console.error("Not yet logged in.");
-            return;
-        } else {
-            Meteor.call('check_mail', function (){});
-        }
-    },
-    "click .portals-filter": function (e, tmpl) {
-        $('#portalTable > .filter-bar').toggleClass('hidden');
-        tmpl.$('.portals-filter').toggleClass('active');
+  "click .refresh": function (e, tmpl) {
+    console.log("Check mail start...");
+    if(!Meteor.userId()){
+      console.error("Not yet logged in.");
+      return;
+    } else {
+      Meteor.call('check_mail', function (){});
     }
+  },
+  "click .portals-filter": function (e, tmpl) {
+    $('#portalTable > .filter-bar').toggleClass('hidden');
+    tmpl.$('.portals-filter').toggleClass('active');
+  },
+  'click .drawer-toggle': function(tpl, e) {
+    $('.sidebar-nav-drawer').drawer('toggle', 150);
+  }
 });
 
 /*Template.portalList.helpers({
