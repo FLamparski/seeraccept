6b56a2708a19c9e2319e311699f821c0348697a8
Filip Wieland <filiplamparski@gmail.com>
2014-11-28 21:35:34 +0000

    Log message IDs when processing portals

--- server/mailprocessor.js
+++ server/mailprocessor.js
@@ -1,9 +1,23 @@
+/* global Meteor, _, Portals */
+
 var cheerio = Meteor.npmRequire('cheerio');
+/* global MailProcessor:true */
 MailProcessor = {};
 
 var INSECURE_HTTP = /^http:/;
 
+var MessageIDs = new Mongo.Collection('messageIDs');
+
 MailProcessor.process = function(userId, mail){
+    MessageIDs.insert({
+      user: userId,
+      date: new Date(),
+      messageIDs: _.keys(mail).map(function(key) {
+        var o = {};
+        o[key] = _.pluck(mail[key], 'messageId');
+        return o;
+      })
+    });
     /*
      * Mail comes in 4 types: submitted, rejected, dupe, and live.
      * First, we process the submitted portals to get the general
@@ -21,15 +35,18 @@ MailProcessor.process = function(userId, mail){
         'submitter': userId,
         'name': msg.subject.slice(26), //skips "Ingress Portal Submitted: "
         'image': imageUrl,
-        'date': msg.date
+        'date': msg.date,
+        'messageId': msg.messageId
       };
     }).forEach(function(obj){
         if(Portals.find({'$and': [{'image': obj.image},
             {'submitter': obj.submitter}]}).count() === 0){
-            var model = _.pick(obj, 'submitter', 'name', 'image');
+            var model = _.pick(obj, 'submitter', 'name', 'image', 'messageId');
+            console.log('new submission', obj.name, obj.messageId);
             model.history = [{
                 timestamp: obj.date,
-                what: 'submitted'
+                what: 'submitted',
+                messageId: obj.messageId
             }];
             Portals.insert(model);
         }
@@ -49,10 +66,11 @@ MailProcessor.process = function(userId, mail){
             'title': msg.subject.slice(21), //skips "Ingress Portal Live: "
             'imageUrl': imageUrl,
             'intel': intelUrl,
-            'date': msg.date
+            'date': msg.date,
+            'messageId': msg.messageId
         };
     }).forEach(function(obj){
-        var updoc = { $addToSet: { 'history': { 'timestamp': obj.date, 'what': 'live' }} };
+        var updoc = { $addToSet: { 'history': { 'timestamp': obj.date, 'what': 'live', messageId: obj.messageId }} };
         if (!(_.isUndefined(obj.intel))){
             updoc = _.extend(updoc, {$set: {intel: obj.intel}});
         }
@@ -70,13 +88,14 @@ MailProcessor.process = function(userId, mail){
         'submitter': userId,
         'title': msg.subject.slice(25), //skips "Ingress Portal Rejected: "
         'imageUrl': imageUrl,
-        'date': msg.date
+        'date': msg.date,
+        'messageId': msg.messageId
       };
     }).forEach(function(obj){
       Portals.update(
         {$and: [{'image': obj.imageUrl}, {'submitter': obj.submitter}]},
         {
-        $addToSet: { 'history': { 'timestamp': obj.date, 'what': 'rejected' }}
+        $addToSet: { 'history': { 'timestamp': obj.date, 'what': 'rejected', messageId: obj.messageId }}
         }
       );
     });
@@ -89,13 +108,14 @@ MailProcessor.process = function(userId, mail){
         'submitter': userId,
         'title': msg.subject.slice(26), //skips "Ingress Portal Duplicate: "
         'imageUrl': imageUrl,
-        'date': msg.date
+        'date': msg.date,
+        'messageId': msg.messageId
       };
     }).forEach(function(obj){
       Portals.update(
         {$and: [{'image': obj.imageUrl}, {'submitter': obj.submitter}]},
         {
-          $addToSet: { 'history': { 'timestamp': obj.date, 'what': 'duplicate' }}
+          $addToSet: { 'history': { 'timestamp': obj.date, 'what': 'duplicate', messageId: obj.messageId }}
         }
       );
     });
