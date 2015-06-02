1a91448ff1f5247abbfc22ccb23b88493e83ecae
Filip Wieland <filiplamparski@gmail.com>
2015-06-02 21:24:40 +0100

    Use the new lock field to render spinners/advice

--- client/loading-template.js
+++ client/loading-template.js
@@ -1,11 +1,12 @@
-/* global Template, _ */
+/* global Template, _, Meteor */
 
 var LOADING_MESSAGES = [
   'It\'s a fine day to submit a portal',
   'Be careful with high concentrations of XM',
   'Submit a duck',
   'Mo portals mo problems',
-  'Remember not to submit military installations'
+  'Remember not to submit military installations',
+  'Portal Reviewer 2 must be stopped'
 ];
 Template.loadingPage.helpers({
   loadingMessage: function() {
@@ -15,6 +16,6 @@ Template.loadingPage.helpers({
 
 Template.noPortals.helpers({
   checkingMail: function() {
-    return Meteor.user().profile.isCheckingMail;
+    return Meteor.user().profile.mailCheck;
   }
 });
